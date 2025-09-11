9b0abf9de3b4024eecc57580ab8a010ab555ce62
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-11 13:18:34 -0300

    Added remove last char button

--- calculator/app.js
+++ calculator/app.js
@@ -2,14 +2,12 @@ const buttons = document.querySelectorAll(".add");
 const textField = document.getElementById("input-field");
 const evalBtn = document.getElementById("calculate");
 const clearBtn = document.getElementById("clear");
+const removeLastBtn = document.getElementById("remove-last");
 
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     const clickedButtonValue = event.target.value;
-    console.log(clickedButtonValue); // Output: 'save' or 'delete' depending on which button is clicked
-
     textField.value += clickedButtonValue;
-    console.log(eval(textField.value));
   });
 });
 
@@ -19,4 +17,8 @@ evalBtn.addEventListener("click", function () {
 
 clearBtn.addEventListener("click", function () {
   textField.value = "";
+});
+
+removeLastBtn.addEventListener("click", function () {
+  textField.value = textField.value.slice(0, -1);
 });
