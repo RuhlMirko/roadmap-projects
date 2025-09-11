35bdd583680c2ae6fa6f318ba031f83b0d6e6057
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-11 01:15:07 -0300

    Buttons add correct values to the input box

---
+++ calculator/app.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll(".add");
+const textField = document.getElementById("input-field");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const clickedButtonValue = event.target.value;
+    console.log(clickedButtonValue); // Output: 'save' or 'delete' depending on which button is clicked
+
+    textField.value += clickedButtonValue;
+    console.log(eval(textField.value));
+  });
+});
