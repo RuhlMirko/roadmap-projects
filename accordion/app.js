2ada017e7151c923273761b4abbf5730058aa3ec
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-14 15:15:32 -0300

    Clicking another question closes all other questions

--- accordion/app.js
+++ accordion/app.js
@@ -2,7 +2,12 @@ const questions = document.querySelectorAll(".question");
 
 questions.forEach((question) => {
   question.addEventListener("click", (e) => {
+    closeQuestions();
     const targetP = e.target.parentNode.children[1];
     targetP.style.display = "block";
   });
 });
+
+function closeQuestions() {
+  questions.forEach((q) => (q.children[1].style.display = "none"));
+}
