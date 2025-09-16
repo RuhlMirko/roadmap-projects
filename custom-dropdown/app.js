8f623a074e1a2ee8b5e0a0f1e0d2f9faaeaa05de
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-16 16:14:40 -0300

    Button now updates with selected item

--- custom-dropdown/app.js
+++ custom-dropdown/app.js
@@ -1,12 +1,28 @@
 "use strict";
 const dropBtn = document.getElementById("dropBtn");
+let currentSelection = "Select an Item";
 let isOpen = false;
 const dropdownContent = document.getElementById("dropdown-content");
+const dropdownItems = dropdownContent.querySelectorAll("#dropdown-content a");
 
 dropBtn.addEventListener("click", () => {
   isOpen = !isOpen;
-  dropBtn.innerHTML = isOpen
-    ? "Select an Item <i class='fa-solid fa-caret-up'></i>"
-    : "Select an Item <i class='fa-solid fa-caret-down'></i>";
   dropdownContent.style.display = isOpen ? "flex" : "none";
+  updateBtn();
 });
+
+dropdownItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentSelection = item.innerHTML;
+    dropdownContent.style.display = "none";
+    isOpen = false;
+    updateBtn();
+  });
+});
+
+function updateBtn() {
+  dropBtn.innerHTML = `${currentSelection} <i class="fa-solid fa-caret-${
+    isOpen ? "up" : "down"
+  }"></i>`;
+}
