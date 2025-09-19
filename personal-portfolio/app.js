3ae46310d7bbef51e48b8abb1f6990115fba9d9d
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-19 12:51:13 -0300

    Updated styling and clipboard button

--- personal-portfolio/app.js
+++ personal-portfolio/app.js
@@ -1,10 +1,10 @@
 function copyEmail() {
-  const email = document.getElementById("email").textContent;
+  const email = "ruhlmirkojoel@gmail.com";
   navigator.clipboard.writeText(email).then(function () {
     const msg = document.getElementById("copiedMsg");
     msg.style.display = "block";
     setTimeout(() => {
       msg.style.display = "none";
-    }, 1500);
+    }, 3500);
   });
 }
